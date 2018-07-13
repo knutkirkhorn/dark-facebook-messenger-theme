e306e0056eee8ea2d9b9c39f6ae689b493b5dbd8
Knut Kirkhorn <knutkirk@hotmail.com>
2018-07-13 15:46:03 +0200

    Add script for changing the favicons and create a installation guide for that

---
+++ favicon-replacer.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name     dark-facebook-messenger-theme
+// @version  1.0.0
+// @grant    none
+// @match http://www.facebook.com/*
+// @match https://www.facebook.com/*
+// @match http://www.messenger.com/*
+// @match https://www.messenger.com/*
+// ==/UserScript==
+
+/**
+*	Made by Knut Kirkhorn - Licensed MIT
+**/
+
+function repalceFavicon(url) {
+    let element = document.createElement('link');
+    element.rel = 'icon';
+    element.href = url;
+    document.head.appendChild(element);
+}
+
+if (window.location.host === 'www.facebook.com') {
+    repalceFavicon('https://raw.githubusercontent.com/Knutakir/dark-facebook-messenger-theme/media/fb.ico');
+} else if (window.location.host === 'www.messenger.com') {
+    repalceFavicon('https://raw.githubusercontent.com/Knutakir/dark-facebook-messenger-theme/media/messenger.ico');
+}
